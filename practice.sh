857a7808bcc04a5f1a458e7774013ba7527133e4
Your Name <you@example.com>
2022-04-11 13:00:20 +0900

    日本のタイムゾーンの指定を追加

--- practice.sh
+++ practice.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# 現在の時刻をAMかPMかを判定して、どちらかを格納するnow変数
-AMorPM=`date +%p`
+# タイムゾーンを日本時間に設定する
+export TZ=JST-9
 
-if [ $AMorPM = "AM" ]
+# 現在の時刻をAMかPMかを判定して、どちらかを格納するnow変数
+now=`date +%p`
+
+if [ $now = "AM" ]
 then
     ls -a > output.txt
-elif [ $AMorPM = "PM" ]
+elif [ $now = "PM" ]
 then
     datetime=`date +%T`
     echo "現在の時刻は"${datetime}
